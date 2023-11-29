c117b67e3039695721bd1fd7c5c91a98912f5dec
kristinwegener <kristinwegener94@gmail.com>
2023-11-29 16:21:55 +0100

    render fetched data

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ const maxPage = 1;
 const page = 1;
 const searchQuery = "";
 
-CharacterCard(
-  "https://pyxis.nymag.com/v1/imgs/3b7/ca7/5fd3353737d602a5a1caa3fce92cb33b39-rick-morty.1x.rsquare.w1400.jpg",
-  "test",
-  "test",
-  "test",
-  "test"
-);
-
 export async function fetchCharacters() {
   const response = await fetch("https://rickandmortyapi.com/api/character");
   const data = await response.json();
   return data.results;
 }
+
+const data = await fetchCharacters();
+
+console.log(data[1].name);
+
+data.forEach((person) => {
+  CharacterCard(person.image, person.name, person.status, person.occurence);
+});
