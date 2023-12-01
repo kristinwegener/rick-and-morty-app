b3ad5786c2e80629cfa63b000abbbdd53247cce0
Ceren Jana Akkuec <cjakkuec@gmail.com>
2023-12-01 15:54:44 +0100

    refactor index.js, CharacterCard.js

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,27 +1,33 @@
 import { cardContainer } from "../../index.js";
 
-export function CharacterCard(imgURL, title, status, type, occurence) {
+export function CharacterCard(character) {
+  const { image, name, status, species, episode } = character;
+  const occurrences = episode.length;
+
   const card = document.createElement("li");
+
   card.innerHTML = `<div class="card__image-container">
                         <img
                         class="card__image"
-                        src="${imgURL}"
-                        alt="${title}"
+                        src="${image}"
+                        alt="${name}"
                         />
                         <div class="card__image-gradient"></div>
                     </div>
                     <div class="card__content">
-                        <h2 class="card__title">${title}</h2>
+                        <h2 class="card__title">${name}</h2>
                         <dl class="card__info">
                         <dt class="card__info-title">Status</dt>
                         <dd class="card__info-description">${status}</dd>
                         <dt class="card__info-title">Type</dt>
-                        <dd class="card__info-description">${type}</dd>
+                        <dd class="card__info-description">${species}</dd>
                         <dt class="card__info-title">Occurrences</dt>
-                        <dd class="card__info-description">${occurence}</dd>
+                        <dd class="card__info-description">${occurrences}</dd>
                         </dl>
                     </div>`;
+
   card.classList.add("card");
   cardContainer.append(card);
+
   return card;
 }
